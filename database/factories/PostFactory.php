fe957b1b6d27012c5478adba98da6cdd6beda3db
Marcy Acevedo <marcy.acevedo@tighten.co>
2022-09-27 12:56:44 -0500

    Add post factory. Start seeding.

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+class PostFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'caption' => fake()->sentence(),
+            'image_url' => fake()->imageUrl(),
+        ];
+    }
+}
--- database/migrations/2022_09_27_141443_create_posts_table.php
+++ database/migrations/2022_09_27_141443_create_posts_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->text('caption')->nullable();
-            $table->string('image_source');
+            $table->string('image_url');
             $table->timestamps();
         });
     }
