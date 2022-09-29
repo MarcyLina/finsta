262cb7765979f15c33a5a1c58fe26038921795cf
Marcy Acevedo <marcy.acevedo@tighten.co>
2022-09-29 17:05:52 -0500

    Stylr post component. style the nav.

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,19 +1,18 @@
-<p>
-    caption: {{ $post->caption }}
-</p>
+<a href="{{ route('post.show', $post->id) }}" class="block p-5 my-6 bg-white border w-[35vw] mx-auto shadow-md">
+    <p class="mb-2 font-bold">
+        {{ $post->user->username }}
+    </p>
 
-<p>
-    post id: {{ $post->id }}
-</p>
+    <img src="{{ asset( $post->image) }}" alt="{{ $post->caption }}" class="object-cover mb-3 border h-96 w-96">
 
-<p class="font-bold">
-    user name: {{ $post->user->name }}
-</p>
+    <p>
+        <span class="font-bold">
+            {{ $post->user->username }}
+        </span>
+        {{ $post->caption }}
+    </p>
 
-<p>
-    date/time posted: {{ $post->created_at }}
-</p>
-
-<a href="{{ route('post.show', $post->id) }}">
-    <img src="{{ asset( $post->image) }}" alt="{{ $post->caption }}" class="object-cover h-96 w-96">
+    <p>
+        date/time posted: {{ $post->created_at }}
+    </p>
 </a>
