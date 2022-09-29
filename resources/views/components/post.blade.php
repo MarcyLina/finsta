e3d3186daadf975127dda78a72917ef86726c3c6
Marcy Acevedo <marcy.acevedo@tighten.co>
2022-09-29 18:06:29 -0500

    Style cleanup

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,9 +1,9 @@
-<a href="{{ route('post.show', $post->id) }}" class="block p-5 my-6 bg-white border w-[35vw] mx-auto shadow-md">
+<a href="{{ route('post.show', $post->id) }}" class="block p-5 mx-auto my-6 bg-white border shadow-md w-[450px]">
     <p class="mb-2 font-bold">
         {{ $post->user->username }}
     </p>
 
-    <img src="{{ asset( $post->image) }}" alt="{{ $post->caption }}" class="object-cover mb-3 border h-96 w-96">
+    <img src="{{ asset( $post->image) }}" alt="{{ $post->caption }}" class="object-cover h-[450px] mb-3 w-[450px]">
 
     <p>
         <span class="font-bold">
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,34 +1,38 @@
-<nav class="fixed top-0 left-0 flex items-center justify-between w-full p-4 bg-white border-b">
-    <a href="{{ route('posts.index') }}">Tinstagram</a>
+<nav class="fixed top-0 left-0 w-full p-4 bg-white border-b">
+    <div class="grid grid-cols-3 items-center max-w-[1300px] mx-auto">
+        <a href="{{ route('posts.index') }}">
+            <img src="{{ asset('/images/tinsta.webp') }}" alt="tinstagram logo" class="w-1/3 h-auto">
+        </a>
 
-    <input type="text" name="search" id="search" placeholder="Search">
+        <input type="text" name="search" id="search" placeholder="Search" class="justify-self-start">
 
-    @if (auth()->user())
-        <div class="flex">
-            <p>
-                Hi {{ Auth::user()->name }}!
-            </p>
+        @if (auth()->user())
+            <div class="flex">
+                <p>
+                    Hi {{ Auth::user()->name }}!
+                </p>
 
-            <a href="{{ route('post.create') }}" class="mx-6">Create a new post</a>
+                <a href="{{ route('post.create') }}" class="mx-6">Create a new post</a>
 
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
 
-                <a
-                    :href="route('logout')"
-                    onclick="event.preventDefault();
-                    this.closest('form').submit();"
-                >
-                    {{ __('Log Out') }}
+                    <a
+                        :href="route('logout')"
+                        onclick="event.preventDefault();
+                        this.closest('form').submit();"
+                    >
+                        {{ __('Log Out') }}
+                    </a>
+                </form>
+            </div>
+        @else
+            <div>
+                <a href="{{ route('login') }}">Log In</a>
+                <a class="text-sm text-gray-600 underline hover:text-gray-900" href="{{ route('register') }}">
+                    {{ __('Need to register?') }}
                 </a>
-            </form>
-        </div>
-    @else
-        <div>
-            <a href="{{ route('login') }}">Log In</a>
-            <a class="text-sm text-gray-600 underline hover:text-gray-900" href="{{ route('register') }}">
-                {{ __('Need to register?') }}
-            </a>
-        </div>
-    @endif
+            </div>
+        @endif
+    </div>
 </nav>
