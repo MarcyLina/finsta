46ff42be0966692135cb523221a27cb15ab273b6
Marcy Acevedo <marcy.acevedo@tighten.co>
2022-09-29 13:25:31 -0500

    Add auth middleware.

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller('App\Http\Controllers\PostController')->group(function () {
     Route::get('/', 'index')->name('posts.index');
-    Route::get('/p/new', 'create')->name('post.create');
-    Route::post('/post-store', 'store')->name('post.store');
-    Route::get('/p/{post}/edit', 'edit')->name('post.edit');
-    Route::put('/update/{post}', 'update')->name('post.update');
-    Route::delete('/delete/{post}', 'destroy')->name('post.destroy');
+    Route::get('/p/new', 'create')->middleware('auth')->name('post.create');
+    Route::post('/post-store', 'store')->middleware('auth')->name('post.store');
+    Route::get('/p/{post}/edit', 'edit')->middleware('auth')->name('post.edit');
+    Route::put('/update/{post}', 'update')->middleware('auth')->name('post.update');
+    Route::delete('/delete/{post}', 'destroy')->middleware('auth')->name('post.destroy');
     Route::get('/p/{id}', 'show')->name('post.show');
 });
 
