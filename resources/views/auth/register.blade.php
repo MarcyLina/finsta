8e239ef87d5b2708bec28baf426496107b3ecda5
Marcy Acevedo <marcy.acevedo@tighten.co>
2022-10-03 14:24:10 -0500

    Update logo. Make menu dropdown. Style finessing.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,9 +1,9 @@
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 text-gray-500 fill-current" />
-            </a>
+            <div class="flex justify-center">
+                <x-application-logo />
+            </div>
         </x-slot>
 
         <!-- Validation Errors -->
