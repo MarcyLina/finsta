86af190d9b17ed43839ee0a8cb9bad47e07a37a9
Marcy Acevedo <marcy.acevedo@tighten.co>
2022-10-03 14:27:04 -0500

    Format alpine directives for better readability.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,16 +5,32 @@
         <input type="text" name="search" id="search" placeholder="Search">
 
         @if (auth()->user())
-            <div x-data="{open: false}" class="relative justify-self-end">
-                <button @click="open = true" class="flex items-baseline border-none rounded-full">
+            <div
+                x-data="{open: false}"
+                class="relative justify-self-end"
+            >
+                <button
+                    @click="open = true"
+                    class="flex items-baseline border-none rounded-full"
+                >
                     <img src="https://www.humanesociety.org/sites/default/files/styles/2000x850/public/2020-07/kitten-510651.jpg?h=f54c7448&itok=lJefJMMQ" alt="" class="object-cover w-16 h-16 bg-blue-300 rounded-full">
 
-                    <svg :class="open ? 'rotate-180' : ''" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-3 transition ease-in-out">
+                    <svg
+                        :class="open ? 'rotate-180' : ''"
+                        class="w-6 h-6 ml-3 transition ease-in-out"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                    >
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                     </svg>
                 </button>
 
-                <div x-cloak x-show="open" @click.outside="open = false" class="absolute w-48 p-4 mt-2 bg-white border">
+                <div
+                    x-cloak
+                    x-transition
+                    x-show="open"
+                    @click.outside="open = false"
+                    class="absolute w-48 p-4 mt-2 bg-white border"
+                >
                     <p class="mb-6 font-bold">
                         Hi {{ Auth::user()->name }}!
                     </p>
